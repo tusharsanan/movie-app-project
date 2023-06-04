8a6ccfbeccdcc29c2df0574b6e55c7bb57576129
Tushar Sanan <tushar.sanan@gmail.com>
2023-06-05 01:20:44 +0200

    refactor(movie-app): corrected IMDB ID

--- src/app/constants/movie-app.constants.ts
+++ src/app/constants/movie-app.constants.ts
@@ -2,4 +2,4 @@ export const GET_MOVIES_BASE_URL: string = 'https://www.omdbapi.com/';
 
 export const API_KEY: string = '6c3a2d45';
 
-export const FEATURED_MOVIES_IDS = ['tt0120737', 'tt111sss9646'];
+export const FEATURED_MOVIES_IDS = ['tt0120737', 'tt1119646'];
